7860d9ddd7c333aeedb9518b2bd3c265f02c9fdf
trepalin-dmitry <trepalin.work@mail.ru>
2021-11-06 00:03:45 +0300

    Class04 v3

--- src/main/java/Class04TaskC.java
+++ src/main/java/Class04TaskC.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class Class04TaskC {
     public static void main(String[] args) throws IOException {
@@ -16,29 +15,22 @@ public class Class04TaskC {
 
     public static String[] calc(BufferedReader bufferedReader) throws IOException {
         int N = Integer.parseInt(bufferedReader.readLine());
+
         Map<Integer, Integer> map = new HashMap<>();
-        AtomicLong result = new AtomicLong();
         for (int i = 0; i < N; i++) {
             int[] line = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int w = line[0];
             int h = line[1];
-            map.compute(w, (key, value) -> {
-                int currValue;
-                if (value == null) {
-                    value = 0;
-                }
-                if (value >= h) {
-                    currValue = value;
-                } else {
-                    currValue = h;
-                    result.addAndGet(h - value);
-                }
-                return currValue;
-            });
+            map.compute(w, (key, oldValue) -> oldValue != null && oldValue >= h ? oldValue : h);
+        }
+
+        long result = 0;
+        for (Integer value : map.values()) {
+            result += (long) value;
         }
 
         return new String[]{
-                String.valueOf(result.get())
+                String.valueOf(result)
         };
     }
 }
